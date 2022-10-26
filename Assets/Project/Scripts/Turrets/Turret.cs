0f70cada3f9fa193d2162229788e89368a7556f4
Eric <eric.ruiz@enti.cat>
2022-10-26 22:50:53 +0200

    basic turret

--- Assets/Project/Scripts/Turrets/Turret.cs
+++ Assets/Project/Scripts/Turrets/Turret.cs
@@ -1,38 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Turret : Building
 {
+    private BoxCollider bc;
+    private Pool attackPool;
+
+    [Header("STATS")]
     [SerializeField] int playCost;
     [SerializeField] int damage;
-    [SerializeField] int shootCooldown;
     [SerializeField] int attackRange;
     [SerializeField] int targetAmount;
-    [SerializeField] List<(int, float)> enemies = new List<(int, float)>();
-    
+    [SerializeField] float shootCooldown;
+    private float currentShootTimer;
+
+    [Header("OTHERS")]
     [SerializeField] private Transform shootingPoint;
+    
+    private List<(int, float)> enemies = new List<(int, float)>();
+
+    private void Awake()
+    {
+        attackPool = GameObject.Find("BasicTurretAttackPool").gameObject.GetComponent<Pool>();
+        currentShootTimer = 0.0f;
+
+        bc = gameObject.GetComponent<BoxCollider>();
+        bc.size = new Vector3(attackRange, 1.0f, attackRange);
+    }
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.B))
+        if(currentShootTimer < shootCooldown)
         {
-            enemies.Add((1, (float)Random.Range(1, 10)));
-            enemies.Sort(mySort);
-            
-            foreach ((int, float) enemy in enemies)
+            currentShootTimer += Time.deltaTime;
+        }
+        else
+        {
+            if (enemies.Count > 0)
             {
-                Debug.Log(enemy.Item1 + ", " + enemy.Item2);
+                currentShootTimer = 0.0f;
+
+                for (int i = 0; i < targetAmount; i++)
+                {
+                    if(i <= enemies.Count - 1)
+                    {
+                        //Shoot(enemies[i].Item2);
+                    }
+                }
             }
         }
+    }
+
+    private void Shoot(Transform enemyTarget)
+    {
+        TurretAttack currentAttack = attackPool.GetObject().gameObject.GetComponent<TurretAttack>();
+        currentAttack.transform.position = shootingPoint.position;
+        currentAttack.gameObject.SetActive(true);
+        currentAttack.Activate(enemyTarget, damage);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Enemy")
         {
-            //Enemy currentEnemy = other.gameObject.GetComponent<Enemy>();
-            //enemies.Enqueue(currentEnemy, currentEnemy.GetDistance());
+            enemies.Add((10, 10.0f));
+            enemies.Sort(mySort);
         }
     }
 
@@ -40,8 +74,17 @@ public class Turret : Building
     {
         if (other.tag == "Enemy")
         {
+            int i = 0;
+            foreach((int, float) enemy in enemies)
+            {
+                //if(enemy.Item2 == other.gameObject.GetComponent<Enemy>())
+                //{
+                //    enemies.RemoveAt(i);
+                //}
+                i++;
+            }
+
             enemies.Sort(mySort);
-            //Enemy currentEnemy = other.gameObject.GetComponent<Enemy>();
         }
     }
 
