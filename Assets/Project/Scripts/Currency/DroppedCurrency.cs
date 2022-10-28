2dc6605ab356aee8c61614d09b6cf9fcc6ac1658
Eric <eric.ruiz@enti.cat>
2022-10-28 23:15:48 +0200

    indicator added

--- Assets/Project/Scripts/Currency/DroppedCurrency.cs
+++ Assets/Project/Scripts/Currency/DroppedCurrency.cs
@@ -2,16 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
 
 public class DroppedCurrency : MonoBehaviour
 {
+    private bool beeingPickedUp;
+
     private int value;
-    
+
+    [Header("COMPONENTS")]
     [SerializeField] private float indicatorTime;
+
+    [Header("COMPONENTS")]
+    [SerializeField] private MeshRenderer meshRenderer;
+    [SerializeField] private RectTransform canvasTransform;
+    [SerializeField] private TMP_Text text;
 
     private void OnMouseEnter()
     {
-        StartCoroutine(GotPickedUp());
+        if(!beeingPickedUp)
+        {
+            StartCoroutine(GotPickedUp());
+        }
+    }
+
+    private void OnEnable()
+    {
+        beeingPickedUp = false;
+        text.gameObject.SetActive(false);
+
+        meshRenderer.enabled = true;
+    }
+
+    private void Update()
+    {
+        Vector3 cameraDirection = Camera.main.transform.forward;
+
+        canvasTransform.rotation = Quaternion.LookRotation(cameraDirection);
     }
 
     public void SetValue(int newValue)
@@ -21,6 +48,13 @@ public class DroppedCurrency : MonoBehaviour
 
     private IEnumerator GotPickedUp()
     {
+        beeingPickedUp = true;
+
+        text.text = "+" + value.ToString();
+        text.gameObject.SetActive(true);
+
+        meshRenderer.enabled = false;
+
         yield return new WaitForSeconds(indicatorTime);
         gameObject.SetActive(false);
     }
