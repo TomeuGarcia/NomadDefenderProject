162d741c1143d2e2a8fa622f9f2195aee7c1448f
Eric <eric.ruiz@enti.cat>
2022-11-03 21:20:48 +0100

    position and fov changes

--- Assets/Project/Scripts/Cards/HandBuildingCards.cs
+++ Assets/Project/Scripts/Cards/HandBuildingCards.cs
@@ -171,13 +171,13 @@ public class HandBuildingCards : MonoBehaviour
 
     private void ComputeSelectedPosition()
     {
-        selectedPosition = (transform.right * (-3f)) + (transform.up * (2f)) + transform.position;
+        selectedPosition = (transform.right * (-2.5f)) + (transform.up * (2f)) + transform.position;
     }
     
     private void ComputeHiddenPosition()
     {
         defaultHandPosition = transform.position;
-        hiddenHandPosition = transform.position + (-0.9f * transform.up);
+        hiddenHandPosition = transform.position + (-0.95f * transform.up);
     }
 
     private void HideHand()
