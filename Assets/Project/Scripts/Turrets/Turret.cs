87d808c87aeffa5e183182a335a61546972f1fff
Tomeu Garcia Pintó <tomeu.garcia@enti.cat>
2022-11-04 22:02:08 +0100

    Turrets only target enemies if they aren't targeted to death
    Also decoupled health HUD

--- Assets/Project/Scripts/Turrets/Turret.cs
+++ Assets/Project/Scripts/Turrets/Turret.cs
@@ -42,27 +42,11 @@ public class Turret : Building
 
     private void Update()
     {
-        if (!isFunctional) return;
-
-        if(currentShootTimer < stats.cadence)
+        if (isFunctional)
         {
-            currentShootTimer += Time.deltaTime;
+            UpdateShoot();
         }
-        else
-        {
-            if (enemies.Count > 0)
-            {
-                currentShootTimer = 0.0f;
 
-                for (int i = 0; i < stats.targetAmount; i++)
-                {
-                    if(i <= enemies.Count - 1)
-                    {
-                        Shoot(enemies[i]);
-                    }
-                }
-            }
-        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -70,9 +54,11 @@ public class Turret : Building
         if (other.tag == "Enemy")
         {
             Enemy enemy = other.gameObject.GetComponent<Enemy>();
-            enemy.OnEnemyDeath += DeleteEnemyFromList;
-            enemies.Add(enemy);
-            enemies.Sort(mySort);
+            if (!enemy.DiesFromQueuedDamage())
+            {
+                AddEnemy(enemy);
+            }
+                
         }
     }
 
@@ -80,8 +66,10 @@ public class Turret : Building
     {
         if (other.tag == "Enemy")
         {
-            other.gameObject.GetComponent<Enemy>().OnEnemyDeath -= DeleteEnemyFromList;
-            DeleteEnemyFromList(other.gameObject.GetComponent<Enemy>());
+            Enemy enemy = other.gameObject.GetComponent<Enemy>();
+
+            if (enemies.Contains(enemy))
+                RemoveEnemy(enemy);
         }
     }
 
@@ -102,6 +90,37 @@ public class Turret : Building
     public void InitStats(TurretStats stats)
     {
         this.stats = stats;
+    }
+
+    private void UpdateShoot()
+    {
+        if (currentShootTimer < stats.cadence)
+        {
+            currentShootTimer += Time.deltaTime;
+            return;
+        }
+
+        if (enemies.Count <= 0) return;
+            
+        
+        currentShootTimer = 0.0f;
+
+        for (int i = 0; i < stats.targetAmount; i++)
+        {
+            if (i <= enemies.Count - 1)
+            {
+                if (enemies[i].DiesFromQueuedDamage())
+                {
+                    RemoveEnemy(enemies[i]);
+                    --i;
+                }
+                else
+                {
+                    Shoot(enemies[i]);
+                }
+            }
+        }
+
     }
 
     protected override void DisableFunctionality()
@@ -149,17 +168,31 @@ public class Turret : Building
 
 
     private void Shoot(Enemy enemyTarget)
-    {
+    {     
         TurretAttack currentAttack = attackPool.GetObject().gameObject.GetComponent<TurretAttack>();
         currentAttack.transform.position = shootingPoint.position;
         currentAttack.transform.parent = attackPool.transform;
         currentAttack.gameObject.SetActive(true);
         currentAttack.Init(enemyTarget, stats.damage);
 
+        enemyTarget.QueueDamage(stats.damage);
+
         enemyTarget.ChangeMat();
     }
 
 
+    private void AddEnemy(Enemy enemy)
+    {
+        enemy.OnEnemyDeath += DeleteEnemyFromList;
+        enemies.Add(enemy);
+        enemies.Sort(mySort);
+    }
+
+    private void RemoveEnemy(Enemy enemy)
+    {
+        enemy.OnEnemyDeath -= DeleteEnemyFromList;
+        DeleteEnemyFromList(enemy);
+    }
 
     private void DeleteEnemyFromList(Enemy enemyToDelete)
     {
