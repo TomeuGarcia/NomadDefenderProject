c2e74edd98178d66739eaa260e7e176f08950ae2
Isaac <isaac.medina@enti.cat>
2023-01-30 20:14:32 +0100

    Started Battle Tutorial and created an encripted Loader / Saver of tutos

---
+++ Assets/Project/Scripts/Tutorials/Battle Tutorials/BattleTutorialManager.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleTutorialManager : MonoBehaviour
+{
+
+    //Needs
+    //CurrencyBackgroundImg (can set alpha to 0)
+    [SerializeField] private GameObject currencyBackgroundImg;
+
+    //Speed Up Button (can set alpha to 0)
+    [SerializeField] private GameObject speedUpButton;
+
+    //DeckUI (can set alpha to 0)
+    [SerializeField] private GameObject deckInterface;
+
+    //Card Drawer -> Canvas -> BackgroundImage (can set alpha to 0)
+    [SerializeField] private GameObject redrawInterface;
+
+    //Cards
+    //List<Cards>cards -> only show cards[0] -> dont show cards[0] stats
+    //Maybe create a Card object to do the animation
+
+    //Get Scripted Sequence
+    [SerializeField] private ScriptedSequence scriptedSequence;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currencyBackgroundImg.GetComponent<CanvasGroup>().alpha = 0;
+        currencyBackgroundImg.SetActive(false);
+
+        speedUpButton.GetComponent<CanvasGroup>().alpha = 0;
+        speedUpButton.SetActive(false);
+
+        deckInterface.GetComponent<CanvasGroup>().alpha = 0;
+        deckInterface.SetActive(false);
+
+        redrawInterface.GetComponent<CanvasGroup>().alpha = 0;
+        redrawInterface.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(Tutorial());
+    }
+
+    IEnumerator Tutorial()
+    {
+
+        yield return new WaitForSeconds(1.0f);
+
+        scriptedSequence.NextLine();
+        yield return new WaitUntil(() => scriptedSequence.IsLinePrinted() == true);
+        yield return new WaitForSeconds(1.0f);
+
+        scriptedSequence.NextLine();
+        yield return new WaitUntil(() => scriptedSequence.IsLinePrinted() == true);
+        yield return new WaitForSeconds(1.0f);
+
+        scriptedSequence.NextLine();
+        yield return new WaitUntil(() => scriptedSequence.IsLinePrinted() == true);
+        yield return new WaitForSeconds(1.0f);
+
+        scriptedSequence.NextLine();
+        yield return new WaitUntil(() => scriptedSequence.IsLinePrinted() == true);
+        yield return new WaitForSeconds(1.0f);
+
+        scriptedSequence.NextLine();
+        yield return new WaitUntil(() => scriptedSequence.IsLinePrinted() == true);
+        yield return new WaitForSeconds(1.0f);
+
+        scriptedSequence.NextLine();
+        yield return new WaitUntil(() => scriptedSequence.IsLinePrinted() == true);
+        yield return new WaitForSeconds(1.0f);
+
+        scriptedSequence.NextLine();
+        yield return new WaitForSeconds(2.0f);
+        //scriptedSequence.Clear();
+    }
+
+}
