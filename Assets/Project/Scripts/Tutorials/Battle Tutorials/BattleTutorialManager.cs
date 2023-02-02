1b6d9aa5174682d2280f3f815741e22207a67496
Isaac <isaacmedrod@hotmail.com>
2023-02-02 20:51:09 +0100

    Finished Battle Tutorial Card Shown

--- Assets/Project/Scripts/Tutorials/Battle Tutorials/BattleTutorialManager.cs
+++ Assets/Project/Scripts/Tutorials/Battle Tutorials/BattleTutorialManager.cs
@@ -20,6 +20,13 @@ public class BattleTutorialManager : MonoBehaviour
     [SerializeField] private GameObject redrawInterface;
 
     //Cards
+    [SerializeField] private GameObject card;
+    [SerializeField] private CanvasGroup attackStat;
+    [SerializeField] private CanvasGroup cadencyStat;
+    [SerializeField] private CanvasGroup rangeStat;
+    [SerializeField] private CanvasGroup projectile;
+    [SerializeField] private CanvasGroup passive;
+    [SerializeField] private CanvasGroup cost;
     //List<Cards>cards -> only show cards[0] -> dont show cards[0] stats
     //Maybe create a Card object to do the animation
 
@@ -40,6 +47,16 @@ public class BattleTutorialManager : MonoBehaviour
 
         redrawInterface.GetComponent<CanvasGroup>().alpha = 0;
         redrawInterface.SetActive(false);
+
+        card.GetComponent<CanvasGroup>().alpha = 0;
+        card.SetActive(false);
+
+        attackStat.alpha = 0;
+        cadencyStat.alpha = 0;
+        rangeStat.alpha = 0;
+        projectile.alpha = 0;
+        passive.alpha = 0;
+        cost.alpha = 0;
 
         StartCoroutine(Tutorial());
     }
@@ -111,26 +128,112 @@ public class BattleTutorialManager : MonoBehaviour
         scriptedSequence.NextLine();
         yield return new WaitUntil(() => scriptedSequence.IsLinePrinted() == true);
 
-        scriptedSequence.NextLine();
-        yield return new WaitUntil(() => scriptedSequence.IsLinePrinted() == true);
+        card.SetActive(true);
+        for (float i = 0.0f; i < 1.0f; i += 0.01f)
+        {
+            card.GetComponent<CanvasGroup>().alpha = i;
+            yield return null;
+        }
+        card.GetComponent<CanvasGroup>().alpha = 1.0f;
+
+        //Cards Stats
+
 
         scriptedSequence.NextLine();
         yield return new WaitUntil(() => scriptedSequence.IsLinePrinted() == true);
+        yield return new WaitForSeconds(0.5f);
 
         scriptedSequence.NextLine();
         yield return new WaitUntil(() => scriptedSequence.IsLinePrinted() == true);
+        yield return new WaitForSeconds(1.0f);
+
+        //Range
+        for (float i = 0.0f; i < 1.0f; i += 0.01f)
+        {
+            rangeStat.GetComponent<CanvasGroup>().alpha = i;
+            yield return null;
+        }
+        rangeStat.GetComponent<CanvasGroup>().alpha = 1.0f;
 
         scriptedSequence.NextLine();
         yield return new WaitUntil(() => scriptedSequence.IsLinePrinted() == true);
+        yield return new WaitForSeconds(1.0f);
+
+        //Cadency
+        for (float i = 0.0f; i < 1.0f; i += 0.01f)
+        {
+            cadencyStat.GetComponent<CanvasGroup>().alpha = i;
+            yield return null;
+        }
+        cadencyStat.GetComponent<CanvasGroup>().alpha = 1.0f;
 
         scriptedSequence.NextLine();
         yield return new WaitUntil(() => scriptedSequence.IsLinePrinted() == true);
+        yield return new WaitForSeconds(1.0f);
+
+        //Attack
+        for (float i = 0.0f; i < 1.0f; i += 0.01f)
+        {
+            attackStat.GetComponent<CanvasGroup>().alpha = i;
+            yield return null;
+        }
+        attackStat.GetComponent<CanvasGroup>().alpha = 1.0f;
 
         scriptedSequence.NextLine();
         yield return new WaitUntil(() => scriptedSequence.IsLinePrinted() == true);
+        yield return new WaitForSeconds(1.0f);
+
+        //Cost
+        for (float i = 0.0f; i < 1.0f; i += 0.01f)
+        {
+            cost.GetComponent<CanvasGroup>().alpha = i;
+            yield return null;
+        }
+        cost.GetComponent<CanvasGroup>().alpha = 1.0f;
 
         scriptedSequence.NextLine();
         yield return new WaitUntil(() => scriptedSequence.IsLinePrinted() == true);
+        yield return new WaitForSeconds(1.0f);
+
+        //Projectile
+        for (float i = 0.0f; i < 1.0f; i += 0.01f)
+        {
+            projectile.GetComponent<CanvasGroup>().alpha = i;
+            yield return null;
+        }
+        projectile.GetComponent<CanvasGroup>().alpha = 1.0f;
+
+        scriptedSequence.NextLine();
+        yield return new WaitUntil(() => scriptedSequence.IsLinePrinted() == true);
+        yield return new WaitForSeconds(1.0f);
+
+        //Passive
+        for (float i = 0.0f; i < 1.0f; i += 0.01f)
+        {
+            passive.GetComponent<CanvasGroup>().alpha = i;
+            yield return null;
+        }
+        passive.GetComponent<CanvasGroup>().alpha = 1.0f;
+
+        scriptedSequence.NextLine();
+        yield return new WaitUntil(() => scriptedSequence.IsLinePrinted() == true);
+        yield return new WaitForSeconds(1.0f);
+
+
+
+        //Erases Card
+        scriptedSequence.NextLine();
+        yield return new WaitUntil(() => scriptedSequence.IsLinePrinted() == true);
+        yield return new WaitForSeconds(1.0f);
+
+        
+        for (float i = 1.0f; i > 0.0f; i -= 0.01f)
+        {
+            card.GetComponent<CanvasGroup>().alpha = i;
+            yield return null;
+        }
+        card.GetComponent<CanvasGroup>().alpha = 0.0f;
+        card.SetActive(false);
 
         //Finishes Cards Tutorial
 
@@ -187,6 +290,16 @@ public class BattleTutorialManager : MonoBehaviour
         scriptedSequence.NextLine();
         yield return new WaitUntil(() => scriptedSequence.IsLinePrinted() == true);
         yield return new WaitForSeconds(0.5f);
+
+        scriptedSequence.NextLine(); //Line 27
+        yield return new WaitUntil(() => scriptedSequence.IsLinePrinted() == true);
+        yield return new WaitForSeconds(1.0f);
+
+        //Falta ensenyar a sel·leccionar una carta
+        //Falta jugar una carta
+
+        //Falta pausar el joc i obligar al jugador a millorar torreta.
+        //Falta error final i finalitzar partida
 
         //Finishes Deck Tutorial
 
