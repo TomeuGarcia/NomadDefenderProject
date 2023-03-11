513fc03f1b8d37fe353eb00eaa6685eb22a4f181
Eric <eric.ruiz@enti.cat>
2023-03-12 00:03:27 +0100

    build fixed

--- Assets/Project/Scripts/Tutorials/OWMapTutorial/OWMapTutorialManager2.cs
+++ Assets/Project/Scripts/Tutorials/OWMapTutorial/OWMapTutorialManager2.cs
@@ -3,10 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
-using Unity.VisualScripting;
 using UnityEngine.Rendering;
 using DG.Tweening;
-using UnityEditor.Rendering.LookDev;
 
 public class OWMapTutorialManager2 : MonoBehaviour
 {
