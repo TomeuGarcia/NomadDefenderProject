efe9bdc2fe0d53f4498fcb75957b92613832fb71
Eric <eric.ruiz@enti.cat>
2023-04-16 23:39:03 +0200

    layout change

--- Assets/Project/Scripts/Upgrades/UpgradeMachineControl.cs
+++ Assets/Project/Scripts/Upgrades/UpgradeMachineControl.cs
@@ -8,8 +8,18 @@ public class UpgradeMachineControl : MonoBehaviour
 {
     [Header("REFERENCES")]
     [SerializeField] private MeshRenderer leftArm;
+    [SerializeField] private MeshRenderer leftCardSlot;
+    [SerializeField] private List<MeshRenderer> leftCables = new List<MeshRenderer>();
+
     [SerializeField] private MeshRenderer rightArm;
+    [SerializeField] private MeshRenderer rightCardSlot;
+    [SerializeField] private List<MeshRenderer> rightCables = new List<MeshRenderer>();
+
     [SerializeField] private MeshRenderer screen;
+    [SerializeField] private MeshRenderer screenButton;
+    [SerializeField] private MeshRenderer screenCardSlot;
+
+
 
     [Header("MATERIAL LERP DATA")]
     [SerializeField] private MaterialLerp.FloatData armOnFD;
@@ -18,50 +28,71 @@ public class UpgradeMachineControl : MonoBehaviour
     [SerializeField] private MaterialLerp.FloatData slotFD;
     [SerializeField] private MaterialLerp.FloatData cableFD;
 
+
+
+    private void Awake()
+    {
+        RenderSettings.reflectionIntensity = 0.0f;
+    }
+    private void OnDestroy()
+    {
+        RenderSettings.reflectionIntensity = 1.0f;
+    }
+
     void Update()
     {
-        //left arm
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            StartCoroutine(MaterialLerp.FloatLerp(armOnFD, new Material[1] { leftArm.materials[1] }));
+            SelectLeftCard();
         }
         if (Input.GetKeyDown(KeyCode.W))
         {
-            StartCoroutine(MaterialLerp.FloatLerp(armOffFD, new Material[1] { leftArm.materials[1] }));
+            RetrieveLeftCard();
         }
 
 
-        //screen
-        //left
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            screenFD.variableReference = "_FirstFillCoef";
-            screenFD.invert = false;
-            screenFD.endGoal = 1;
-            StartCoroutine(MaterialLerp.FloatLerp(screenFD, new Material[1] { screen.materials[2] }));
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            screenFD.variableReference = "_FirstFillCoef";
-            screenFD.invert = true;
-            screenFD.endGoal = 0;
-            StartCoroutine(MaterialLerp.FloatLerp(screenFD, new Material[1] { screen.materials[2] }));
-        }
-        //right
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            screenFD.variableReference = "_SecondFillCoef";
-            screenFD.invert = false;
-            screenFD.endGoal = 1;
-            StartCoroutine(MaterialLerp.FloatLerp(screenFD, new Material[1] { screen.materials[2] }));
-        }
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            screenFD.variableReference = "_SecondFillCoef";
-            screenFD.invert = true;
-            screenFD.endGoal = 0;
-            StartCoroutine(MaterialLerp.FloatLerp(screenFD, new Material[1] { screen.materials[2] }));
-        }
+        ////left arm
+        //if (Input.GetKeyDown(KeyCode.Q))
+        //{
+        //    StartCoroutine(MaterialLerp.FloatLerp(armOnFD, new Material[1] { leftArm.materials[1] }));
+        //}
+        //if (Input.GetKeyDown(KeyCode.W))
+        //{
+        //    StartCoroutine(MaterialLerp.FloatLerp(armOffFD, new Material[1] { leftArm.materials[1] }));
+        //}
+
+
+        ////screen
+        ////left
+        //if (Input.GetKeyDown(KeyCode.A))
+        //{
+        //    screenFD.variableReference = "_FirstFillCoef";
+        //    screenFD.invert = false;
+        //    screenFD.endGoal = 1;
+        //    StartCoroutine(MaterialLerp.FloatLerp(screenFD, new Material[1] { screen.materials[2] }));
+        //}
+        //if (Input.GetKeyDown(KeyCode.S))
+        //{
+        //    screenFD.variableReference = "_FirstFillCoef";
+        //    screenFD.invert = true;
+        //    screenFD.endGoal = 0;
+        //    StartCoroutine(MaterialLerp.FloatLerp(screenFD, new Material[1] { screen.materials[2] }));
+        //}
+        ////right
+        //if (Input.GetKeyDown(KeyCode.D))
+        //{
+        //    screenFD.variableReference = "_SecondFillCoef";
+        //    screenFD.invert = false;
+        //    screenFD.endGoal = 1;
+        //    StartCoroutine(MaterialLerp.FloatLerp(screenFD, new Material[1] { screen.materials[2] }));
+        //}
+        //if (Input.GetKeyDown(KeyCode.F))
+        //{
+        //    screenFD.variableReference = "_SecondFillCoef";
+        //    screenFD.invert = true;
+        //    screenFD.endGoal = 0;
+        //    StartCoroutine(MaterialLerp.FloatLerp(screenFD, new Material[1] { screen.materials[2] }));
+        //}
     }
 
     public void SelectLeftCard()
