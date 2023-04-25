a7b32fd189cb80199de7f332717f718edd41ff14
Eric <eric.ruiz@enti.cat>
2023-04-25 20:32:04 +0200

    incomplete card build fix

--- Assets/Project/Scripts/Upgrades/UpgradeNodeFeedback/IncompleteCard.cs
+++ Assets/Project/Scripts/Upgrades/UpgradeNodeFeedback/IncompleteCard.cs
@@ -6,10 +6,31 @@ public class IncompleteCard : MonoBehaviour
 {
     [SerializeField] private List<TextDecoder> decoders = new List<TextDecoder>();
 
+    IEnumerator Start()
+    {
+        DeactivateAllDecoders();
+        yield return new WaitForSeconds(0.5f);
+        ActivateAllDecoders();
+    }
+
     private void OnEnable()
     {
-        foreach(TextDecoder d in decoders)
+        ActivateAllDecoders();
+    }
+
+    private void DeactivateAllDecoders()
+    {
+        foreach (TextDecoder d in decoders)
         {
+            d.gameObject.SetActive(false);
+        }
+    }
+
+    private void ActivateAllDecoders()
+    {
+        foreach (TextDecoder d in decoders)
+        {
+            d.gameObject.SetActive(true);
             d.Activate();
         }
     }
