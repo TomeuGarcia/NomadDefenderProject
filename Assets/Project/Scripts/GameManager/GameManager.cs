cf5ffe87077ccc49d3c20aed8e0167c1ae2851a9
TomeuGarcia <tomeu.garcia@enti.cat>
2023-05-06 21:45:17 +0200

    Updated game over / victory

--- Assets/Project/Scripts/GameManager/GameManager.cs
+++ Assets/Project/Scripts/GameManager/GameManager.cs
@@ -11,6 +11,12 @@ public class GameManager : MonoBehaviour
     [SerializeField] protected GameObject victoryHolder;
     [SerializeField] protected GameObject gameOverHolder;
 
+    [Header("TEXTS")]
+    [SerializeField] protected TextDecoder victoryTitleTextDecoder;
+    [SerializeField] protected TextDecoder victorySubtitleTextDecoder;
+    [SerializeField] protected TextDecoder gameOverTitleTextDecoder;
+    [SerializeField] protected TextDecoder gameOverSubtitleTextDecoder;
+
     [Header("DEPENDENCIES")]
     [SerializeField] private OverworldMapGameManager overworldMapGameManager;
     [SerializeField] private MapSceneLoader mapSceneLoader;
@@ -46,17 +52,34 @@ public class GameManager : MonoBehaviour
     }
     private IEnumerator DoStartVictory()
     {
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(2.0f);
+        victoryTitleTextDecoder.Activate();
+
+        yield return new WaitForSeconds(1.0f);
+        victorySubtitleTextDecoder.Activate();
+
+
         SceneLoader.GetInstance().StartLoadGameEndCredits();
+        GameAudioManager.GetInstance().ChangeMusic(GameAudioManager.MusicType.MENU, 3.0f);
     }
 
 
     private void StartGameOver()
     {
         gameOverHolder.SetActive(true);
+
+        StartCoroutine(DoStartGameOver());
+    }
+    private IEnumerator DoStartGameOver()
+    {
+        yield return new WaitForSeconds(2.0f);
+        gameOverTitleTextDecoder.Activate();
+
+        yield return new WaitForSeconds(1.0f);
+        gameOverSubtitleTextDecoder.Activate();
+
         mapSceneLoader.LoadMainMenuScene(5f);
     }
-
 
 
 }
