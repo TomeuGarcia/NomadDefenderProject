5d78bb47ee1e757763913eefc06302c5ff0c0a36
TomeuGarcia <tomeu.garcia@enti.cat>
2024-07-29 13:16:06 +0200

    Fix Clone and StockGenerator

--- Assets/Project/Scripts/Turrets/Parts/Passives/DrawCard/SpawnCardCopyInDeck.cs
+++ Assets/Project/Scripts/Turrets/Parts/Passives/DrawCard/SpawnCardCopyInDeck.cs
@@ -7,35 +7,15 @@ using UnityEngine;
     menuName = SOAssetPaths.TURRET_PARTS_BASEPASSIVES + "DeploySpawnCopyInDeck")]
 public class SpawnCardCopyInDeck : BasePassive
 {
-    private TurretBuilding owner;
-
     [Space]
     [SerializeField, Min(0)] private int costIncrement = 50;
+    [SerializeField] private SpawnCardCopyInDeckBehaviour _behaviourPrefab;
 
 
     public override void ApplyEffects(TurretBuilding owner)
     {
-        this.owner = owner;
-        owner.OnGotPlaced += SpawnCardCopy;
-        owner.OnDestroyed += UnsubscriveEvents;        
-    }
-
-    private void UnsubscriveEvents()
-    {
-        owner.OnGotPlaced -= SpawnCardCopy;
-        owner.OnDestroyed -= UnsubscriveEvents;
-    }
-
-
-    private async void SpawnCardCopy()
-    {
-        await Task.Delay(500);
-
-        TurretCardParts partsCopy = ScriptableObject.CreateInstance("TurretCardParts") as TurretCardParts;
-        partsCopy.InitCopyingReferences(owner.TurretCardParts);
-        partsCopy.cardCost += costIncrement;
-
-        ServiceLocator.GetInstance().CardDrawer.SpawnTurretCardInDeck(partsCopy);
+        SpawnCardCopyInDeckBehaviour behaviour = Instantiate(_behaviourPrefab, owner.transform);
+        behaviour.Init(owner, costIncrement);        
     }
 
 }
