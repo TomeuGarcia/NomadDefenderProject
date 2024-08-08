31af7aa41664434d1fd12a67fdc723dd5789e17d
Eric <eric.ruiz@enti.cat>
2024-08-09 00:23:49 +0200

    Better owmapnode communication

--- Assets/Project/Scripts/Utilities/Materials/MaterialProperties.cs
+++ Assets/Project/Scripts/Utilities/Materials/MaterialProperties.cs
@@ -6,4 +6,6 @@ public static class MaterialProperties
 {
     public static string Activate => "_Activate";
     public static string Deactivate => "_Deactivate";
+    public static string Appear => "_Appear";
+    public static string Disappear => "_Disappear";
 }
