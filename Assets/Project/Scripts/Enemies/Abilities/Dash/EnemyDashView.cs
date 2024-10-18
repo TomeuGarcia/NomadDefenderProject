d8747cf8e1133607c317a46ce5d00bc74776586f
Eric <eric.ruiz@enti.cat>
2024-10-18 11:39:26 +0200

    Template Factory

--- Assets/Project/Scripts/Enemies/Abilities/Dash/EnemyDashView.cs
+++ Assets/Project/Scripts/Enemies/Abilities/Dash/EnemyDashView.cs
@@ -3,17 +3,27 @@ using UnityEngine;
 public class EnemyDashView : MonoBehaviour
 {
     [SerializeField] private ParticleSystem _dashParticles;
-    [SerializeField] private GameObject _dashSphere;
+    [SerializeField] private ProgressiveRotator _progressiveRotator;
+    [SerializeField] private float _dashRotation;
     
     public void StartDashing()
     {
         _dashParticles.Play();
-        _dashSphere.SetActive(true);
+
+        _progressiveRotator.enabled = false;
+        _progressiveRotator.transform.localRotation = Quaternion.identity;
     }   
     
     public void StopDashing()
     {
         _dashParticles.Stop();
-        _dashSphere.SetActive(false);
-    }        
+
+        _progressiveRotator.enabled = true;
+    }
+
+    private void OnDisable()
+    {
+        _progressiveRotator.enabled = true;
+        _progressiveRotator.transform.localRotation = Quaternion.identity;
+    }
 }
