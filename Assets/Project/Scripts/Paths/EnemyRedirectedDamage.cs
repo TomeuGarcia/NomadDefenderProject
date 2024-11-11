6e63468f01f61950e93862b0fdc671b8c010c428
TomeuGarcia <tomeu.garcia.p@gmail.com>
2024-11-11 16:07:29 +0100

    Update RedirectedEnemyDamage trail time

--- Assets/Project/Scripts/Paths/EnemyRedirectedDamage.cs
+++ Assets/Project/Scripts/Paths/EnemyRedirectedDamage.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
@@ -7,19 +8,27 @@ public class EnemyRedirectedDamage : MonoBehaviour
 {
     [SerializeField] private AnimationCurve _movementEase = AnimationCurve.Linear(0,0, 1, 1);
     [SerializeField] private ParticleSystem _reachTargetParticles;
+    [SerializeField] private TrailRenderer _trailRenderer;
     
     private Enemy _enemySource;
     private PathLocation _originLocation;
     private PathLocation _targetLocation;
 
-    private const float MOVE_SPEED = 12.0f;
+    private const float MOVE_SPEED = 10.0f;
     private static Vector3 CONTROL_POINTS_OFFSET = Vector3.up * 3.0f;
 
     private Timer _moveTimer;
     private Vector3 _startPoint;
     private Vector3 _endPoint;
-        
-    
+
+    private float _defaultTrailTime;
+
+    private void Awake()
+    {
+        _defaultTrailTime = _trailRenderer.time;
+    }
+
+
     public void Init(Enemy enemySource, PathLocation originLocation, PathLocation targetLocation)
     {
         _enemySource = enemySource;
@@ -41,6 +50,7 @@ public class EnemyRedirectedDamage : MonoBehaviour
         while (!_moveTimer.HasFinished())
         {
             _moveTimer.Update(Time.unscaledDeltaTime * Mathf.Min(Time.timeScale, 2.0f));
+            _trailRenderer.time = _defaultTrailTime / Mathf.Max(Time.timeScale, 0.1f);
             UpdatePosition();
             yield return null;
         }
