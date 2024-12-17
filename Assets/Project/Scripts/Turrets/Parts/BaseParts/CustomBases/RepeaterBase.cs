70cbd3e807f565da13dff74a5d8bd1111d77376b
TomeuGarcia <tomeu.garcia.p@gmail.com>
2024-12-17 16:42:27 +0100

    Starter deck update + changes

--- Assets/Project/Scripts/Turrets/Parts/BaseParts/CustomBases/RepeaterBase.cs
+++ Assets/Project/Scripts/Turrets/Parts/BaseParts/CustomBases/RepeaterBase.cs
@@ -59,7 +59,7 @@ public class RepeaterBase : TurretPartBase_Prefab
         fakeEnemy.gameObject.SetActive(false);
 
         HideAllTurretBinders();
-        currentDamagePer1Increment = 0f;
+        currentDamagePer1Increment = damagePer1Increments[0];
         repeatAreaPlaneMaterial = repeatAreaPlane.materials[0];
     }
 
@@ -168,7 +168,7 @@ public class RepeaterBase : TurretPartBase_Prefab
             UpdateAreaPlaneSize(ownerSupportBuilding, repeatAreaPlane, repeatAreaPlaneMaterial);
         }
 
-        currentDamagePer1Increment = damagePer1Increments[currentLvl - 1];        
+        currentDamagePer1Increment = damagePer1Increments[currentLvl];        
     }
     
     public override void ResetAreaPlaneSize(SupportBuilding supportOwner)
