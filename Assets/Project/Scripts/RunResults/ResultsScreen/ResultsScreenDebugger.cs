d491b4b4f57511a3e59708df816d7da32063fc14
TomeuGarcia <tomeu.garcia.p@gmail.com>
2025-01-08 15:59:46 +0100

    Update RunResultsScreen

--- Assets/Project/Scripts/RunResults/ResultsScreen/ResultsScreenDebugger.cs
+++ Assets/Project/Scripts/RunResults/ResultsScreen/ResultsScreenDebugger.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class ResultsScreenDebugger : MonoBehaviour
 { 
+    [SerializeField] private bool _enabled;
     [SerializeField] private bool _victory;
     [SerializeField] private RunState _runState;
     [SerializeField] private CardDeckAsset _starterDeck;
@@ -10,6 +11,8 @@ public class ResultsScreenDebugger : MonoBehaviour
 
     private void Awake()
     {
+        if (!_enabled) return;
+        
         _runState.Init(_starterDeck, null);
         _runState.DebugOverwriteWithRandomData(_victory, _starterDeck, _starterDeck.MakeDeckContent(), _mostDamagingEnemy);
     }
