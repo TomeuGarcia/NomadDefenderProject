926b068ec4473fff490cd5f9d1cf65917df84c68
TomeuGarcia <tomeu.garcia.p@gmail.com>
2025-01-23 19:00:27 +0100

    Fixing run results

--- Assets/Project/Scripts/RunResults/ResultsScreen/ResultsScreenObjectPreviewer.cs
+++ Assets/Project/Scripts/RunResults/ResultsScreen/ResultsScreenObjectPreviewer.cs
@@ -24,12 +24,11 @@ public class ResultsScreenObjectPreviewer : MonoBehaviour
     
     public void InitToShow(Camera camera, GameObject objectToPreview, string appendedText)
     {
-        _text.SetTextStrings(_text.textComponent.text += '\n' + appendedText);
+        _text.SetTextStrings(_text.textComponent.text += '\n' + "<size=150%>"+ appendedText + "</size>");
         
-        if(objectToPreview.TryGetComponent(out Enemy enemy))
+        if(objectToPreview.GetComponent<Enemy>() != null)
         {
             FindDespicableObjects(objectToPreview.transform);
-            enemy.PositionWithCenteredMesh();
         }
 
         _previewImage.enabled = false;
@@ -84,5 +83,10 @@ public class ResultsScreenObjectPreviewer : MonoBehaviour
         _text.Activate();
         yield return new WaitUntil(() => _text.FinishedLine);
     }
-    
+
+    public void CompleteShowAnimation()
+    {
+        _objectToPreview.SetActive(true);
+        _text.SetStringInstantly();
+    }
 }
