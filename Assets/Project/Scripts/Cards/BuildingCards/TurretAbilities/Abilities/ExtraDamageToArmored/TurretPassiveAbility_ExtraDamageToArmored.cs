eaa92bcc772d6b957a07c1a8765f0eeafd16999c
TomeuGarcia <tomeu.garcia.p@gmail.com>
2025-02-07 18:53:22 +0100

    Update after playtest

--- Assets/Project/Scripts/Cards/BuildingCards/TurretAbilities/Abilities/ExtraDamageToArmored/TurretPassiveAbility_ExtraDamageToArmored.cs
+++ Assets/Project/Scripts/Cards/BuildingCards/TurretAbilities/Abilities/ExtraDamageToArmored/TurretPassiveAbility_ExtraDamageToArmored.cs
@@ -1,5 +1,7 @@
 
 
+using UnityEngine;
+
 public class TurretPassiveAbility_ExtraDamageToArmored : ATurretPassiveAbility
 {
     private readonly float _damageMultiplier;
@@ -19,7 +21,7 @@ public class TurretPassiveAbility_ExtraDamageToArmored : ATurretPassiveAbility
     {
         if (damageAttack.Target.HealthSystem.HasArmor())
         {
-            damageAttack.UpdateDamage((int)(damageAttack.Damage * _damageMultiplier));
+            damageAttack.UpdateDamage(Mathf.RoundToInt(damageAttack.Damage * _damageMultiplier));
             _projectileParticleFactory.CreateParticlesGameObject(ProjectileParticleType.MetalCruncher_Hit,
                 damageAttack.Target.Position, damageAttack.ProjectileSource.Rotation);
         }
