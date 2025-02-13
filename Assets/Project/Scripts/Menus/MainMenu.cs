81ad486f7c1689eef36d99246cd040c1a6857ef1
Eric <eric.ruiz@enti.cat>
2025-02-13 16:57:32 +0100

    demoAraSi100p100_2_asdasd (04)

--- Assets/Project/Scripts/Menus/MainMenu.cs
+++ Assets/Project/Scripts/Menus/MainMenu.cs
@@ -137,6 +137,7 @@ public class MainMenu : MonoBehaviour
 
     private void Update()
     {
+        return;
         if (Input.GetKeyDown(KeyCode.F))
         {
             skipFirstBattle = true;
