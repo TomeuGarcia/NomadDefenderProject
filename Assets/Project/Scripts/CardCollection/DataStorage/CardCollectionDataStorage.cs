9cef767ad490a8cf83983b67f4d6bf95766cefc5
TomeuGarcia <tomeu.garcia.p@gmail.com>
2025-03-11 12:11:04 +0100

    Prepare for dev build

--- Assets/Project/Scripts/CardCollection/DataStorage/CardCollectionDataStorage.cs
+++ Assets/Project/Scripts/CardCollection/DataStorage/CardCollectionDataStorage.cs
@@ -174,9 +174,9 @@ public class CardCollectionDataStorage : ScriptableObject
             return;
         }
         
-        ResetDiscoveries();
         if (!Directory.Exists(PathToFile_StreamingAssets) || !File.Exists(PathToFile_StreamingAssets + FILE_NAME))
         {
+            ResetDiscoveries();
             Directory.CreateDirectory(PathToFile_StreamingAssets);
             SaveData(false);
         }
